6d0df5580fa98f9a94b569806c42f6c91d89ecee
Zarivate <Zarivate1@gmail.com>
2022-07-28 18:44:22 -0500

    Attempt at api calls made

--- frontend/src/components/restaurants-list.js
+++ frontend/src/components/restaurants-list.js
@@ -2,8 +2,96 @@ import React, { useState, useEffect } from "react";
 import RestaurantDataService from "../services/restaurant";
 import { Link } from "react-router-dom";
 
-function RestaurantsList() {
-  return <div>RestaurantsList component goes here</div>;
-}
+const RestaurantsList = (props) => {
+  // States to hold all the possible search/data parameters needed
+  const [restaurants, setRestaurants] = useState([]);
+  const [searchName, setSearchName] = useState("");
+  const [searchZip, setSearchZip] = useState("");
+  const [searchCuisine, setSearchCuisine] = useState("");
+  const [cuisines, setCuisines] = useState(["All Cuisines"]);
+
+  // Function to retrieve the restaurants and cuisines right at start
+  useEffect(() => {
+    retrieveRestaurants();
+    retrieveCuisines();
+  }, []);
+
+  // Functions to change state search parameters
+  const onChangeSearchName = (e) => {
+    const searchName = e.target.value;
+    setSearchName(searchName);
+  };
+
+  const onChangeSearchZip = (e) => {
+    const searchZip = e.target.value;
+    setSearchZip(searchZip);
+  };
+
+  const onChangeSearchCuisine = (e) => {
+    const searchCuisine = e.target.value;
+    setSearchCuisine(searchCuisine);
+  };
+
+  // Functions to make calls to api call function in RestaurantDataService, in this case would retrieve all the restaurants from database
+  const retrieveRestaurants = () => {
+    RestaurantDataService.getAll()
+      .then((response) => {
+        console.log(response.data);
+        setRestaurants(response.data.restaurants);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const retrieveCuisines = () => {
+    RestaurantDataService.getCuisines()
+      .then((response) => {
+        console.log(response.data);
+        // Since a drop down menu for cuisines is used, data is instead added to the state until there is no more cuisine data to add
+        setCuisines(["All Cuisines"].concat(response.data));
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  // Function to refresh the list of restaurants displayed
+  const refreshList = () => {
+    retrieveRestaurants();
+  };
+
+  // Function called by other find functions below it,
+  const find = (query, by) => {
+    RestaurantDataService.find(query, by)
+      .then((response) => {
+        console.log(response.data);
+        setRestaurants(response.data.restaurants);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  // Make call to find function, with the name parameter value from the state of "searchName".
+  const findByName = () => {
+    // Categorized as "name" so knows which api call to make in restaurant.js
+    find(searchName, "name");
+  };
+
+  const findByZip = () => {
+    find(searchZip, "zipcode");
+  };
+
+  const findByCuisine = () => {
+    if (searchCuisine == "All Cuisines") {
+      refreshList();
+    } else {
+      find(searchCuisine, "cuisine");
+    }
+  };
+
+  return <div>Howdy</div>;
+};
 
 export default RestaurantsList;
